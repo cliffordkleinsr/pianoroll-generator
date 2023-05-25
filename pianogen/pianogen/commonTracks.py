079128c7a230d7935d3f04529feff3b17a220a2c
Cliff Njoroge <37869706+cliffordkleinsr@users.noreply.github.com>
2023-05-25 11:26:47 +0300

    Update commonTracks.py

--- pianogen/pianogen/commonTracks.py
+++ pianogen/pianogen/commonTracks.py
@@ -1,49 +1,71 @@
-import glob as glob, shutil,os, json
+import glob as glob
+import shutil
+import os
+import json
 from os.path import join
 from pypianoroll import Multitrack
-''' pypianoroll == 0.5.3!!!!'''
 
 class MidiFileProcessor(Multitrack):
-    def __init__(
-        self,
-        fldr,
-        #savez,
-    ) -> None:
-        self.fldr = fldr # the folder where your midis are saved
-        #self.savez = savez
+    def __init__(self, fldr):
+        """
+        Initialize the MidiFileProcessor object.
 
-    def get_items(
-        self,
-    ) -> None:
+        Parameters:
+        - fldr: The folder where the MIDI files are saved.
+        """
+        self.fldr = fldr
+        # self.savez = savez
+
+    def get_items(self):
+        """
+        Get a list of MIDI filenames from the specified folder.
+
+        Returns:
+        - files: List of MIDI filenames.
+        """
         files = glob.glob(join(self.fldr, '*mid'))
         return files
-        
-    def most_common(
-        self,
-        lst,
-    ) -> None:
+
+    def most_common(self, lst):
+        """
+        Find the most common element in a list.
+
+        Parameters:
+        - lst: List of elements.
+
+        Returns:
+        - The most common element in the list.
+        """
         return max(set(lst), key=lst.count)
-    
-    def get_listItems(
-        self,
-    ) -> None:
-        #os.makedirs(self.savez, exist_ok=True)
+
+    def get_listItems(self):
+        """
+        Process the MIDI files and save a list of common files.
+
+        Returns:
+        None
+        """
+        # Create the savez folder if it doesn't exist
+        # os.makedirs(self.savez, exist_ok=True)
+
+        # Get the list of MIDI files
         files = self.get_items()
+
+        # Get the number of tracks for each MIDI file
         tracks = [len(Multitrack(f).tracks) for f in files]
+
+        # Find the most common number of tracks
         mc = self.most_common(tracks)
 
-        '''loop'''
+        # Iterate over the MIDI files
         common = []
         for f in files:
             m = Multitrack(f)
             if len(m.tracks) == mc:
-                common +=[f]
-                #print('moving:', f, 'with no. of tracks ', len(m.tracks))
-                #shutil.move(f, self.savez)
+                common.append(f)
+                # Move the file to the savez folder
+                # shutil.move(f, self.savez)
+
+        # Save the list of common files to a JSON file
         with open('common_files.json', 'w') as outfile:
-            json.dump(common, outfile)      
-            
-        
-        
-                  
-        
+            json.dump(common, outfile)
