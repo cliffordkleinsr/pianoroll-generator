75bc729311a2b98d123e450cf69a761cbd8778bb
Cliff Njoroge <37869706+cliffordkleinsr@users.noreply.github.com>
2023-05-25 11:24:40 +0300

    Update parse_to_npy.py

--- pianogen/pianogen/parse_to_npy.py
+++ pianogen/pianogen/parse_to_npy.py
@@ -1,57 +1,67 @@
-import glob as glob, numpy as np, json
+import glob as glob
+import numpy as np
+import json
 from os.path import join
 from pypianoroll import Multitrack
 
 class MidiParser(Multitrack):
-    def __init__(
-        self,
-        #fldr, # The list MIDI filenames
-        resulting_fname, # The resulting filename
-    ) -> None:
-        
-        #self.fldr = fldr # the folder where your midis are saved
+    def __init__(self, resulting_fname):
+        """
+        Initialize the MidiParser object.
+
+        Parameters:
+        - resulting_fname: The resulting filename.
+        """
         self.resulting_fname = resulting_fname
-        #self.N_TRACKS = [len(Multitrack(f).tracks) for f in self.fldr][0]
-        self.BEAT_RESOLUTION : int = 12
-        self.NUM_BAR : int = 4
-        self.results = []
-        
-    def get_items(
-        self,
-    ) -> None:
-        
-        #all_files = glob.glob(join(self.fldr, '*mid'))
-        with open('common_files.json','r') as opfile:
-            common_files = json.load(opfile)   
+        self.BEAT_RESOLUTION: int = 12  # Beat resolution for the MIDI files
+        self.NUM_BAR: int = 4  # Number of bars for the pianoroll
+        self.results = []  # List to store the parsed results
+
+    def get_items(self):
+        """
+        Get a list of MIDI filenames from a JSON file.
+
+        Returns:
+        - common_files: List of MIDI filenames.
+        """
+        with open('common_files.json', 'r') as opfile:
+            common_files = json.load(opfile)
         return common_files
 
-    def parse(
-        self,
-    ) -> None:
-        N_TRACKS = [len(Multitrack(f).tracks) for f in self.get_items()][0]
-        ## -- Params --
-        RESHAPE_PARAMS = {    # Data
-                'num_bar': self.NUM_BAR,
-                'num_pitch': 84,
-                'num_track': N_TRACKS,
-                'num_timestep': 48,
-                'beat_resolution': self.BEAT_RESOLUTION,
-        }
-        
-        for filename in self.get_items():
-            # Parse the MIDI file into multitrack pianoroll
-            try:
-                multitrack  = Multitrack(filename, beat_resolution=self.BEAT_RESOLUTION)
-            except:
-                continue
+    def parse(self):
+        """
+        Parse MIDI files and generate the resulting pianoroll.
 
-            # Pad to multtple
+        Returns:
+        None
+        """
+        # Get the number of tracks in the MIDI files
+        N_TRACKS = [len(Multitrack(f).tracks) for f in self.get_items()][0]
+
+        # Reshape parameters
+        RESHAPE_PARAMS = {
+            'num_bar': self.NUM_BAR,
+            'num_pitch': 84,
+            'num_track': N_TRACKS,
+            'num_timestep': 48,
+            'beat_resolution': self.BEAT_RESOLUTION,
+        }
+
+        # Iterate over each MIDI file
+        for filename in self.get_items():
+            try:
+                # Parse the MIDI file into a multitrack pianoroll
+                multitrack = Multitrack(filename, beat_resolution=self.BEAT_RESOLUTION)
+            except:
+                continue  # Skip if parsing fails
+
+            # Pad the multitrack pianoroll to a multiple of the beat resolution
             multitrack.pad_to_multiple(4 * self.BEAT_RESOLUTION)
 
             # Binarize the pianoroll
             multitrack.binarize()
 
-            # Sort the tracks according to program number
+            # Sort the tracks according to the program number
             multitrack.tracks.sort(key=lambda x: x.program)
 
             # Bring the drum track to the first track
@@ -60,29 +70,31 @@ class MidiParser(Multitrack):
             # Get the stacked pianoroll
             pianoroll = multitrack.get_stacked_pianoroll()
 
-            # Check length
+            # Check if the pianoroll has enough length
             if pianoroll.shape[0] < 4 * 4 * self.BEAT_RESOLUTION:
-                continue
+                continue  # Skip if too short
 
             # Keep only the mid-range pitches
             pianoroll = pianoroll[:, 24:108]
 
-            # Reshape and get the phrase pianorolls
+            # Reshape the pianoroll into phrases
             pianoroll = pianoroll.reshape(-1, 4 * self.BEAT_RESOLUTION, 84, N_TRACKS)
+
+            # Append the reshaped pianoroll to the results list
             self.results.append(np.concatenate(
                 [pianoroll[:-3], pianoroll[1:-2], pianoroll[2:-1], pianoroll[3:]], 1))
 
-            result = np.concatenate(self.results, 0)
-            
-            print('Original shape: ', result.shape)
-            
-            result = result.reshape(-1, RESHAPE_PARAMS['num_bar'], RESHAPE_PARAMS['num_timestep'],
-                        RESHAPE_PARAMS['num_pitch'], RESHAPE_PARAMS['num_track'])
+        # Concatenate the results into a single array
+        result = np.concatenate(self.results, 0)
 
-            print('New shape: ', result.shape)
-            
-        # NOTE: You might want to shuffle the training data here
-        np.savez_compressed(
-            self.resulting_fname, nonzero=np.array(result.nonzero()),
-            shape=result.shape
-            )
+        print('Original shape:', result.shape)
+
+        # Reshape the result array
+        result = result.reshape(-1, RESHAPE_PARAMS['num_bar'], RESHAPE_PARAMS['num_timestep'],
+                                RESHAPE_PARAMS['num_pitch'], RESHAPE_PARAMS['num_track'])
+
+        print('New shape:', result.shape)
+
+        # Save the resulting pianoroll to a compressed npz file
+        np.savez_compressed(self.resulting_fname, nonzero=np.array(result.nonzero()),
+                            shape=result.shape)
